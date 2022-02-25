50da5a916cd688f45d78a7f66188acf394799170
Ashley Likes <alikes@wsutech.edu>
2022-02-25 11:11:48 -0600

    Added exercises 2-1, 2-2, 8-1, and 8-2

---
+++ pythonProject/exercises/exercise1.py
@@ -0,0 +1,40 @@
+print('Hello World')
+
+# Can you make a typo that generates an error?
+# I can make a type that creates an error.
+# Can you make sense of the error message?
+# I can understand the error message.
+# Can you make a typo that does not generate an error?
+# I can make a typo that does not generate an error.
+# Why do you think it didn't make an error?
+# The error still runs, but it's not as clean of a code
+# Error is End of statement expected.
+# ',')' expected
+# Information on The Zen of Python
+# A list of things a programmer should consider when writing code
+number = "5"
+print(number)
+color = "blue"
+print(color)
+
+
+# I assigned a message to a variable and printed the message.
+# I assigned a message, number, to a variable, 5.
+# Then I printed the message.
+# I assigned another message, color, to a new variable, red,  and printed the message.
+# Then I changed the value of the variable from red to blue and printed the new message again.
+
+def display_message():
+    """Displays chapter content"""
+    print("This chapter is about functions.")
+display_message()
+
+def favorite_book(title):
+    """Display the title of my favorite book."""
+    print(f"My favorite book is {title.title()}.")
+favorite_book('The Velveteen Rabbit')
+
+# On 8-1 I wrote a function display that prints a sentence.
+# I named my function display_message and included a message to print.
+# On 8-2 I added "title" to the function definition.
+# Then I called the function by typing the title of the book, Velveteen Rabbit.
