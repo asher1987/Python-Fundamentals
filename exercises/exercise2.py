390f78d73a34ed336ce72e37d046a9ceb2238452
Ashley Likes <alikes@wsutech.edu>
2022-03-01 12:08:20 -0600

    CAC-102-exercise 2 assignment

---
+++ exercises/exercise2.py
@@ -0,0 +1,54 @@
+# This is the first question in Lab Exercise-My Application Overview
+
+
+def simple(nicknames):
+    """Displays a common nickname"""
+    print(f'I have several nicknames. The most common one is, {nicknames.title()}.')
+
+
+simple('Asher')
+
+
+def simple(nickname_2):
+    """Displays nickname 2"""
+    print(f"Another nickname I have is, {nickname_2}.")
+
+
+simple('Asher Dash')
+
+
+def simple(nickname_3):
+    """Displays nickname 3"""
+    print(f"My friends like to call me, {nickname_3}.")
+
+
+simple('Ash')
+
+
+def simple(nickname_4):
+    """Displays nickname 4 """
+    print(f"My kids call me mom, mommy and {nickname_4}.")
+
+
+simple('momma')
+
+# This is the second question in the lab
+
+def simple2(last_name):
+    """Displays nickname information"""
+    print(f"My name is Ashley, {last_name}.")
+    print(f"My full name is Ashley Kristine,{last_name}.")
+    print(f"My daughter's name is Adelynn, {last_name}.")
+    print(f"My son's name is Aiden, {last_name}.")
+
+
+simple2('Likes')
+
+
+# This is the third question for the assignment
+
+def message(first_name):
+    print(first_name + " Likes ")
+
+
+message('Ashley')
