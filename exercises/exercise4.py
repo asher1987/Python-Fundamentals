3b831d44ea68914bd598e9162cd81ba754cea4ef
Ashley Likes <alikes@wsutech.edu>
2022-03-03 15:14:01 -0600

    CAC-102-5

---
+++ exercises/exercise4.py
@@ -0,0 +1,18 @@
+# Lab Exercise-Numbers
+# Try it Yourself 2-8
+# Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+# Be sure to enclose your operations in print() calls to see the results. you should create four lines.
+# Your output should be simply four lines with the number 8 each appearing once in each line.
+
+print(5 + 3)
+print(16/2)
+print(16-8)
+print(2*4)
+
+# Try it Yourself 2-9
+# Use a variable to represent your favorite number. Then, using the variable, create a message that
+# reveals your favorite number. Print that message.
+
+favorite_number = "3"
+message = f"My favorite number is {favorite_number.title()}."
+print(message)
