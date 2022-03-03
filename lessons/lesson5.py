8a2e0e6d40b6ba207e6cc376839e80b376d9e541
Ashley Likes <alikes@wsutech.edu>
2022-03-03 15:55:35 -0600

    CAC-102-6

---
+++ lessons/lesson5.py
@@ -0,0 +1,56 @@
+# Lesson 5 Operators
+
+# Arithmetic Operators
+# These operators handle basic math.
+
+# Addition
+def add_example():
+    result = 10 + 5
+    print(result)
+
+# Subtraction
+def subtract_example():
+    result2 = 23 - 4
+    print(result2)
+
+
+# add_example()
+# subtract_example()
+
+# Multiplication & Division
+def multi_divide(sum1, sum2):
+    result_multi = sum1 * sum2
+    result_divide = sum1 / sum2
+    print(result_multi)
+    print(result_divide)
+
+
+# multi_divide(10, 5)
+
+
+# Modulus
+def modulus_example(value):
+    result_modulus = value % 24
+    print(result_modulus)
+
+# Exponentation
+
+
+def exponent_example(value):
+    result_exponent = value **3
+    print(result_exponent)
+
+# modulus_example(189)
+# exponent_example(21)
+able = 10
+beta = 765
+
+
+# Floor Division
+def floor_example():
+    result_floor = beta // able
+    print(result_floor)
+
+
+floor_example()
+
