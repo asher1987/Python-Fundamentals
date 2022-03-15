8cbd1fc8cfc2e1eed72073d904cf1da305394655
Ashley Likes <alikes@wsutech.edu>
2022-03-15 16:12:45 -0500

    boat exercise

---
+++ exercises/boat.py
@@ -0,0 +1,45 @@
+# In your boat.py file, write your boat object using your notes from the Intro to Programming class.
+# Refine your attributes for your constructor.
+# Utilize the property() along with the getter and setter for each of your variable attributes.
+# Be sure to test your class and properties.
+class Boat:
+    def __init__(self, color='blue'):
+        self._color = color
+
+    # getter method
+    def get_color(self):
+        return self._color
+
+    # setter method
+    def set_color(self, x):
+        self._age = x
+
+
+ashley = Boat()
+
+print(ashley.get_color())
+print(ashley._color)
+
+
+#property
+class Boat:
+    def __init__(self):
+        self._color = 'blue'
+
+    def get_color(self):
+        return self._color
+
+    def set_color(self, y):
+        self._age = y
+
+    def del_color(self):
+        del self._color
+
+    color = property(get_color, set_color, del_color)
+
+ashley = Boat()
+ashley.color = 'blue'
+
+ashley = Boat()
+print(ashley.get_color())
+print(ashley._color)
